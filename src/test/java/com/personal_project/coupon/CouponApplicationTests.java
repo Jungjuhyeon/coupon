22dfce33f137f142a1d9e77db81d4f90ea51ca13
jungjoo <wjddis000507@gmail.com>
2025-03-07 17:21:52 +0900

    feat : initial commit

---
+++ src/test/java/com/personal_project/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal_project.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
