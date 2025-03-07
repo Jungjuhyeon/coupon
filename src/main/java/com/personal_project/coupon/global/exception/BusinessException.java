4e4b3b1113860aa172f1bdc2b685e396c7b70c34
jungjoo <wjddis000507@gmail.com>
2025-03-08 05:08:04 +0900

    feat : 공통 응답&예외처리

---
+++ src/main/java/com/personal_project/coupon/global/exception/BusinessException.java
@@ -0,0 +1,11 @@
+package com.personal_project.coupon.global.exception;
+
+import com.personal_project.coupon.global.exception.errorcode.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class BusinessException extends RuntimeException{
+    private final ErrorCode errorCode;
+}
