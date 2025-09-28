c4bb24d15727fc945f4c334d36c70fa93339db8a
jungjoo <wjddis000507@gmail.com>
2025-09-29 02:24:33 +0900

    feat: 가게 정보 조회 API 구현
    - StoreInfoOutputDTO 내 `Map<MenuCategory, List<Menu>>` 생성
    - `MenuCategoryResponseDTO.mapToDTO()`로 메뉴를 카테고리 단위로 그룹화
    - `menu_category` 조회 시 `default_batch_fetch_size: 200` 적용 → N+1 문제 해결

--- src/main/java/com/personal_project/coupon/global/exception/errorcode/CommonErrorCode.java
+++ src/main/java/com/personal_project/coupon/global/exception/errorcode/CommonErrorCode.java
@@ -34,11 +34,28 @@ public enum CommonErrorCode implements ErrorCode{
     PROMOTION_NOT_FOUND(HttpStatus.NOT_FOUND,"4101","해당 이벤트를 찾을 수 없습니다."),
     PROMOTION_NOT_ACTIVE(HttpStatus.NOT_FOUND,"4102","이벤트 기간이 아닙니다."),
 
-    //coupon error(4201 ~ 4300)
+    //coupon error(4201 ~ 4250)
     COUPON_NOT_FOUND(HttpStatus.NOT_FOUND,"4201","해당 쿠폰를 찾을 수 없습니다."),
     COUPON_NOT_ACTIVE(HttpStatus.NOT_FOUND,"4202","쿠폰 발급 기간이 아닙니다."),
     COUPON_OUT_OF_STOCK(HttpStatus.NOT_FOUND,"4203", "쿠폰 재고가 부족합니다."),
     COUPON_ALREADY_ISSUED(HttpStatus.BAD_REQUEST, "4204", "이미 쿠폰을 발급받았습니다."),
+
+    //coupon issue error(4251 ~ 4300)
+    COUPON_ALREADY_USED(HttpStatus.BAD_REQUEST, "4251", "쿠폰을 이미 사용했습니다."),
+    COUPON_EXPIRED(HttpStatus.BAD_REQUEST, "4252", "쿠폰 사용기간이 아닙니다."),
+
+
+    //store error(4301 ~ 4400)
+    STORE_NOT_FOUND(HttpStatus.NOT_FOUND,"4301","해당 가게는 존재하지 않습니다."),
+
+    //store_category error(4401 ~ 4450)
+    STORE_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND,"4401","해당 카테고리는 존재하지 않습니다."),
+
+    //brand error(4451 ~ 4500)
+    BRAND_NOT_FOUND(HttpStatus.NOT_FOUND,"4451","해당 브랜드는 존재하지 않습니다."),
+
+    //menu error(4551 ~ 4600)
+    MENU_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND,"4551","해당 메뉴카테고리는 존재하지 않습니다."),
 
     REDIS_SCRIPT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "5001", "Redis 스크립트 실행 중 오류가 발생했습니다."),
     LOCK_ACQUISITION_FAILED(HttpStatus.CONFLICT, "5002", "분산 락 획득에 실패했습니다.");
